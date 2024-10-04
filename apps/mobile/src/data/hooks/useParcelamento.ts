250d1257beb5c5874cdf1d1d27db59ec5e75583d
Felippe Neves <felippenevesmonteiro@gmail.com>
2024-10-03 23:22:06 -0300

    first commit

---
+++ apps/mobile/src/data/hooks/useParcelamento.ts
@@ -0,0 +1,8 @@
+import { CalcularParcelamento } from "@gstore/core";
+
+export default function useParcelamento(
+  valor: number,
+  quantidade: number = 12
+) {
+  return new CalcularParcelamento().executar(valor, quantidade);
+}
---
+++ packages/core/src/parcelamento/index.ts
@@ -0,0 +1,6 @@
+import CalcularParcelamento from "./CalcularParcelamento";
+import Parcelamento from "./Parcelamento";
+
+export { CalcularParcelamento };
+//Para exportar uma interface é preciso do 'type'
+export type { Parcelamento };
